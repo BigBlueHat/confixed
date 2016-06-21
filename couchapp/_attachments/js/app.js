74d52ca5ea54700e5bdacf367535c90ec00a7f90
BigBlueHat <byoung@bigbluehat.com>
2016-06-20 21:28:14 -0400

    Add the all important Resolve button!

--- couchapp/_attachments/js/app.js
+++ couchapp/_attachments/js/app.js
@@ -16,17 +16,7 @@ var app = new Vue({
     }
   },
   ready: function() {
-    var self = this;
-    this.$http.get('_view/conflicts')
-      .then(function(resp) {
-        if (resp.data.total_rows > 0) {
-          resp.data.rows.forEach(function(row) {
-            self.doc_ids.push(row.id);
-            self.conflicts[row.id] = row.value;
-          });
-        }
-      })
-      .catch(console.log.bind(console));
+    this.loadConflicts();
   },
   watch: {
     current: function(id) {
@@ -56,14 +46,56 @@ var app = new Vue({
           })
           .catch(console.log.bind(console));
       }
+    }
+  },
+  methods: {
+    resolve: function() {
       var self = this;
-      this.$http.get('../../'
-          + encodeURIComponent(this.current)
-          + '?rev=' + rev)
+      var bulk = {
+        all_or_nothing: true,
+        docs: []
+      };
+      // add new doc revision from left side
+      bulk.docs.push(JSON.parse(leftInputView.codemirror.getValue()));
+      // add the `delete` request docs for the conflicted doc ids
+      for (var i = 0; i < this.conflicts[this.current].length; i++) {
+        bulk.docs.push({
+          _id: this.current,
+          _rev: this.conflicts[this.current][i],
+          _deleted: true
+        });
+      }
+      this.$http
+        .post('../../_bulk_docs', bulk)
         .then(function(resp) {
-          // pulled from json-diff
-          rightInputView.codemirror
-            .setValue(JSON.stringify(resp.data, null, "\t"));
+          alert(resp.statusText);
+          // keep this console.log until there's UI
+          console.log(resp);
+          self.loadConflicts();
+        })
+        .catch(console.log.bind(console));
+    },
+    resetDefaults: function() {
+      // reset to defaults
+      this.$set('current', '');
+      this.$set('current_rev', '');
+      this.$set('doc_ids', []);
+      this.$set('conflicts', {});
+      // TODO: use Vue for these bits too
+      leftInputView.codemirror.setValue('');
+      rightInputView.codemirror.setValue('');
+    },
+    loadConflicts: function() {
+      var self = this;
+      this.resetDefaults();
+      this.$http.get('_view/conflicts')
+        .then(function(resp) {
+          if (resp.data.total_rows > 0) {
+            resp.data.rows.forEach(function(row) {
+              self.doc_ids.push(row.id);
+              self.conflicts[row.id] = row.value;
+            });
+          }
         })
         .catch(console.log.bind(console));
     }
